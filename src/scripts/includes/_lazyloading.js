7560949ed12182b700ac37fe8801b2be64987411
mundisyum <rodionov134@gmail.com>
2022-03-26 07:17:12 +0300

    Update lazyloading; add youtube video

--- src/scripts/includes/_lazyloading.js
+++ src/scripts/includes/_lazyloading.js
@@ -1,4 +1,4 @@
 import lozad from 'lozad'
 
-const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+const observer = lozad('.lozad', {loaded: ()=>console.log('зуй')}); // lazy loads elements with default selector as '.lozad'
 observer.observe();
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,9 +1,4 @@
 import '../styles/style.scss'
 import './includes/_lazyloading'
 import './includes/_plyr-player'
-import delay from './includes/delay.ts'
-
-(async () => {
-    await delay(3000)
-    console.log('Hello world')
-})()
+import './includes/_youtube-players'
