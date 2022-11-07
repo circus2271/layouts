a35fe77e71b3b35935a86ba8230130abdc6b12e3
mundisyum <rodionov134@gmail.com>
2022-11-08 02:11:38 +0300

    temp

--- src/scripts/includes/_floating-action-button.js
+++ src/scripts/includes/_floating-action-button.js
@@ -1,57 +1,60 @@
-import { distinctUntilChanged, fromEvent, map, filter, throttleTime, tap } from 'rxjs'
+import { distinctUntilChanged, fromEvent, map, filter, throttleTime } from 'rxjs'
+import { isMobile } from './helpers'
 
-let prevScroll
-let buttonVisible = true
-let ignoreScrollEvents = false
-fromEvent(document, 'scroll', { passive: true })
-  .pipe(
-    throttleTime(50),
-    filter(() => ignoreScrollEvents === false),
-    map(() => {
-      const currentScroll = window.scrollY
+const initializeFloatingActionButton = () => {
+  let prevScroll
+  let buttonVisible = true
+  let ignoreScrollEvents = false
 
-      if (currentScroll - prevScroll > 30) {
-        buttonVisible = false
-      }
+  fromEvent(document, 'scroll', { passive: true })
+    .pipe(
+      throttleTime(50),
+      filter(() => ignoreScrollEvents === false),
+      map(() => {
+        const currentScroll = window.scrollY
 
-      if (currentScroll + 30 < prevScroll) {
-        buttonVisible = true
-      }
+        if (currentScroll - prevScroll > 30) {
+          buttonVisible = false
+        }
 
-      prevScroll = currentScroll
-      return buttonVisible
-    }),
-    distinctUntilChanged(),
-  )
-  .subscribe({
-    next: buttonVisible => {
-      if (buttonVisible) {
-        floatingButton.classList.remove('hidden')
-      }
-      if (buttonVisible === false) {
-        floatingButton.classList.add('hidden')
-      }
-    },
-    error: console.log
-  })
+        if (currentScroll + 30 < prevScroll) {
+          buttonVisible = true
+        }
 
-const floatingButton = document.querySelector('.js-floating-action-button'),
-  mobileFullScreenMenu = document.querySelector('.js-mobile-fullscreen-menu')
+        prevScroll = currentScroll
+        return buttonVisible
+      }),
+      distinctUntilChanged(),
+    )
+    .subscribe({
+      next: buttonVisible => {
+        if (buttonVisible) {
+          floatingButton.classList.remove('hidden')
+        }
+        if (buttonVisible === false) {
+          floatingButton.classList.add('hidden')
+        }
+      },
+      error: console.log
+    })
 
-mobileFullScreenMenu.classList.add('initialize-transitions');
+  const floatingButton = document.querySelector('.js-floating-action-button'),
+    mobileFullScreenMenu = document.querySelector('.js-mobile-fullscreen-menu')
 
-floatingButton.onclick = () => {
-  floatingButton.classList.toggle('active')
-  mobileFullScreenMenu.classList.toggle('visible')
-  document.body.classList.toggle('overflow-hidden')
-}
+  mobileFullScreenMenu.classList.add('initialize-transitions')
 
-const videoPlayers = document.querySelectorAll('.js-player')
+  floatingButton.onclick = () => {
+    floatingButton.classList.toggle('active')
+    mobileFullScreenMenu.classList.toggle('visible')
+    document.body.classList.toggle('overflow-hidden')
+  }
 
-// TODO: rewrite with .map + join('')
-const html = [...videoPlayers].reduce((previousValue, currentValue) => {
-  const { videoTitle, videoId } = currentValue.dataset
-  const markup = `
+  const videoPlayers = document.querySelectorAll('.js-player')
+
+  const html = [...videoPlayers].map(player => {
+    const { videoTitle, videoId } = player.dataset
+
+    const markup = `
     <li>
       <a href="#${videoId}">
         ${videoTitle}
@@ -59,37 +62,50 @@ const html = [...videoPlayers].reduce((previousValue, currentValue) => {
     </li>
   `
 
-  return previousValue + markup
-}, ``)
+    return markup
+  }).join('')
 
-const videoTitlesList = document.querySelector('.js-fullscreen-menu__video-titles-list')
-videoTitlesList.innerHTML = html
+  const videoTitlesList = document.querySelector('.js-fullscreen-menu__video-titles-list')
+  videoTitlesList.innerHTML = html
 
-fromEvent(window, 'popstate')
-  .pipe(
-    filter(() => !!window.location.hash),
-    map(() => {
-      const possibleVideoId = window.location.hash.slice(1),
-        playerEl = document.querySelector(`[data-video-id="${possibleVideoId}"]`),
-        distanceToPlayer = playerEl.getBoundingClientRect().top,
-        distanceWithGap = distanceToPlayer - 20
+  fromEvent(window, 'popstate')
+    .pipe(
+      filter(() => !!window.location.hash),
+      map(() => {
+        const possibleVideoId = window.location.hash.slice(1),
+          playerEl = document.querySelector(`[data-video-id="${possibleVideoId}"]`),
+          distanceToPlayer = playerEl.getBoundingClientRect().top,
+          distanceWithGap = distanceToPlayer - 20
 
-      return distanceWithGap
-    })
-  ).subscribe({
-  next: scrollDistance => {
-    floatingButton.classList.remove('active')
-    mobileFullScreenMenu.classList.remove('visible')
-    document.body.classList.remove('overflow-hidden')
-    ignoreScrollEvents = true
-    window.scrollBy(0, scrollDistance)
+        return distanceWithGap
+      })
+    ).subscribe({
+    next: scrollDistance => {
+      floatingButton.classList.remove('active')
+      mobileFullScreenMenu.classList.remove('visible')
+      document.body.classList.remove('overflow-hidden')
+      ignoreScrollEvents = true
+      window.scrollBy(0, scrollDistance)
 
-    setTimeout(() => {
-      // hack using event loop
-      // this code will be executed not immediately, but after short delay
-      // "on next tick"
-      ignoreScrollEvents = false
-    }, 0)
-  },
-  error: console.log
-})
+      setTimeout(() => {
+        // hack using event loop
+        // this code will be executed not immediately, but after short delay
+        // "on next tick"
+        ignoreScrollEvents = false
+      }, 0)
+    },
+    error: console.log
+  })
+}
+
+let initialized = false;
+const setup = () => {
+  if (isMobile() && !initialized) {
+    initialized = true;
+    initializeFloatingActionButton()
+  }
+}
+
+
+setup()
+window.onresize = () => setup();
---
+++ src/scripts/includes/helpers.js
@@ -0,0 +1,2 @@
+
+export const isMobile = () => !!navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|ipod|android|webos|blackberry|windows phone)/gi)
