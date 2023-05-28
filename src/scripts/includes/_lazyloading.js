24ecfdc628c9eeaf0b6677230f94316430046b9f
circus2271 <rodionov134@gmail.com>
2023-05-29 01:59:55 +0300

    update stuff

--- src/scripts/includes/_lazyloading.js
+++ src/scripts/includes/_lazyloading.js
@@ -1,15 +1,5 @@
 import lozad from 'lozad'
 
-const observer = lozad('.lozad',
-  {
-    loaded: function (el) {
-      const withPlaceholder = el.classList.contains('js-lozad-with-sqip-placeholder')
-
-      if (withPlaceholder) {
-        el.classList.add('loaded')
-      }
-    }
-  }
-)
+const observer = lozad('.lozad');
 
 observer.observe()
