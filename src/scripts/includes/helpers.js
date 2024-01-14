c57094409c51f97ff4c438962c8c8f1ecfdab96a
circus2271 <rodionov134@gmail.com>
2024-01-14 14:42:09 +0300

    improve "isMobile" getter function

--- src/scripts/includes/helpers.js
+++ src/scripts/includes/helpers.js
@@ -1,5 +1,15 @@
 import { fromEvent, map, share } from 'rxjs'
 
-export const isMobile = () => !!navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|ipod|android|webos|blackberry|windows phone)/gi)
+const createIsMobileGetter = () => {
+  const findOut = () => !!navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|ipod|android|webos|blackberry|windows phone)/gi)
+
+  let mobile = findOut()
+
+  window.addEventListener('resize', () => mobile = findOut())
+  return () => mobile
+}
+
+export const isMobile = createIsMobileGetter()
 
 export const scroll$ = fromEvent(document, 'scroll', { passive: true }).pipe(map(() => window.scrollY), share() )
+
