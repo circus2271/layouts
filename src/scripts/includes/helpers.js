79875711dad973bf733a65df1e58c6d06306a973
circus2271 <p@WIN-R6PSOGHCMGP.localdomain>
2025-03-06 23:09:06 +0300

    improve mobile device detection

--- src/scripts/includes/helpers.js
+++ src/scripts/includes/helpers.js
@@ -1,15 +1,22 @@
 import { fromEvent, map, share } from 'rxjs'
 
-const createIsMobileGetter = () => {
-  const findOut = () => !!navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|ipod|android|webos|blackberry|windows phone)/gi)
 
-  let mobile = findOut()
+class MobileDeviceDetector {
 
-  window.addEventListener('resize', () => mobile = findOut())
-  return () => mobile
+  constructor() {
+    this.isMobile = this.checkIfDeviceIsMobile()
+    window.addEventListener('resize', () => this.isMobile = this.checkIfDeviceIsMobile())
+  }
+
+  checkIfDeviceIsMobile() {
+    return !!navigator.userAgent && navigator.userAgent.match(/(iphone|ipad|ipod|android|webos|blackberry|windows phone)/gi)
+  }
 }
 
-export const isMobile = createIsMobileGetter()
+const mobileDeviceDetector = new MobileDeviceDetector()
 
+export const isMobile = () => mobileDeviceDetector.isMobile
+
+
+///
 export const scroll$ = fromEvent(document, 'scroll', { passive: true }).pipe(map(() => window.scrollY), share() )
-
